204ad5b8345f05e7d873813716030b569c47c7cf
Colin <colingilbert86@gmail.com>
2016-06-04 03:22:21 -0600

    Improve and modularize stuff.

--- script-pump.cpp
+++ script-pump.cpp
@@ -7,7 +7,20 @@
 #include <boost/filesystem.hpp>
 #include <NetworkHost.hpp>
 
-struct handler_functor
+
+static std::vector<uint32_t> hosts_to_init;
+
+
+struct connection_handler_functor
+{
+	void operator()(uint32_t i)
+	{
+		hosts_to_init.push_back(i);
+	}
+};
+
+
+struct message_handler_functor
 {
 	void operator()(const std::string& s)
 	{
@@ -15,15 +28,42 @@ struct handler_functor
 	}
 };
 
+typedef noob::network_host<message_handler_functor, connection_handler_functor> nethost;
+
+std::string load_file(const std::string& path)
+{
+
+	fmt::MemoryWriter ww;
+	std::ifstream f(path);
+	std::stringstream buffer;
+	buffer << f.rdbuf();
+	return buffer.str();
+}
+
+
+// The tag argument refers to the short word prepended to the file. This is so that the other host knows what to do with it.a
+void send_file(const boost::filesystem::path& p, const std::string& tag, nethost& net)
+{
+
+	fmt::MemoryWriter ww;
+	std::string file_contents = load_file(p.generic_string());
+	ww << tag << ": " << file_contents;
+	std::vector<uint32_t> h;
+	net.send(h, ww.str(), 2, true);
+
+	fmt::MemoryWriter log;
+	log << "[ScriptPump] Broadcasting " << p.generic_string() << ", tag = " << tag;
+	std::cout << log.str() << std::endl;
+}
+
 
 int main(int argc, char* argv[])
 {
-	boost::filesystem::path init("init.as");
-	boost::filesystem::path update("update.as");
-	boost::filesystem::path current_cmd("current_cmd.as");
+	boost::filesystem::path init("init");
+	boost::filesystem::path update("update");
+	boost::filesystem::path current_cmd("current_cmd");
 
-
-	noob::network_host<handler_functor> network;
+	nethost network;
 
 	std::string host_addr = "localhost";
 	uint16_t port_num = 4242;
@@ -37,63 +77,46 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
+
+	boost::system::error_code ec;
+	std::time_t last_write_init, last_write_update, last_write_current_cmd;
+	last_write_init = last_write_update = last_write_current_cmd = 0;
+
 	while (true)
 	{
 		network.tick();
-		//{
-		//	fmt::MemoryWriter ww;
-		//	ww << "[ScriptPump] Attempting to load " << init.generic_string();
-		//	std::cout << ww.str() << std::endl;
-		//}
-
-		if (!boost::filesystem::exists(init))
+		
+		std::time_t temp_last_write = boost::filesystem::last_write_time(init, ec);
+		if (ec == 0)
 		{
-		//	fmt::MemoryWriter ww;
-		//	ww << "[ScriptPump] File " << init.generic_string() << " does not exist!";
-		//	std::cout << ww.str() << std::endl;
-		}
-		else
-		{
-			if (!boost::filesystem::is_regular_file(init))
+			if (temp_last_write > last_write_init)
 			{
-		//		fmt::MemoryWriter ww;
-		//		ww << "[ScriptPump] Could not use " << init.generic_string() << " as it is not a regular file.";
-		//		std::cout << ww.str() << std::endl;
-			}
-			else
-			{
-				boost::system::error_code ec;
-				std::time_t last_write = boost::filesystem::last_write_time(init, ec);
-				auto current_time = std::chrono::system_clock::now();
-				if (last_write > std::chrono::system_clock::to_time_t(current_time))
-				{
-					if (ec != 0)
-					{
-		//				fmt::MemoryWriter ww;
-		//				ww << "[ScriptPump] Error getting to file at " << init.generic_string() << ", " <<  ec.message();
-		//				std::cout << ww.str() << std::endl;
-					}
-					else
-					{	
-						fmt::MemoryWriter ww;
-						std::ifstream f(init.generic_string());
-						std::stringstream buffer;
-						buffer << f.rdbuf();
-						ww << "INIT: " << buffer.str();
-						std::vector<uint32_t> h;
-						network.send(h, ww.str(), 2, true);
-
-						fmt::MemoryWriter log;
-						log << "[ScriptPump] Sending file " << init.generic_string();
-						std::cout << log.str() << std::endl;
-					}
-
-					// current_time isn't so current anymore but that's okay.
-					last_write = std::chrono::system_clock::to_time_t(current_time);
-
-					std::this_thread::sleep_for(std::chrono::milliseconds(25));
-				}
+				send_file(init, "INIT", network);
+				last_write_init = temp_last_write;
 			}
 		}
+
+		temp_last_write = boost::filesystem::last_write_time(update, ec);
+		if (ec == 0)
+		{
+			if (temp_last_write > last_write_update)
+			{
+				send_file(update, "UPDATE", network);
+				last_write_update = temp_last_write;
+			}
+		}
+		
+		temp_last_write = boost::filesystem::last_write_time(current_cmd, ec);
+		if (ec == 0)
+		{
+			if (temp_last_write > last_write_current_cmd)
+			{
+				send_file(current_cmd, "CMD", network);
+				last_write_current_cmd = temp_last_write;
+			}
+		}
+
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(25));
 	}
 }
